bfdfd7d53ae3c128591affc9b5f4737455caabaf
Pragya Saxena <pragyasaxena8279@gmail.com>
2024-08-22 12:16:39 +0530

    Bug Fixes and UI updates

--- public/index.js
+++ public/index.js
@@ -1,13 +1,11 @@
 function deleteJob(id) {
-    const res = confirm('Are you sure you want to delete this job post?');
-    if (res) {
+    const confirmation = confirm('Are you sure you want to delete this job post?');
+    if (confirmation) {
         fetch('/jobs/job-details/' + id, {
             method : 'DELETE'
         }).then(res => {
             window.open('/jobs');
         })
-    } else {
-
     }
 }
 
@@ -15,4 +13,25 @@ function openJobDetails(jobId) {
     const currentUrl = window.location.href;
     const newUrl = `${currentUrl}/job-details/${jobId}`;
     window.open(newUrl, '_blank');
+}
+
+function handleLogoutUser() {
+    const confirmation = confirm('Are you sure you want to logout?');
+    if (confirmation) {
+        fetch('/logout', {
+            method: 'GET',
+        })
+            .then(response => {
+                if (response.ok) {
+                    // Redirect to the login page or homepage after logout
+                    window.location.href = '/'; 
+                } else {
+                    alert('Logout failed. Please try again.');
+                }
+            })
+            .catch(error => {
+                console.error('Error logging out:', error);
+                alert('An error occurred. Please try again.');
+            });
+    }
 }
